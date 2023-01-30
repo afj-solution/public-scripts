36ae5e7231a611e868284d6a3dd1bee4723d384e
TomashGombosh <blackjneco@gmail.com>
2023-01-30 19:17:05 +0200

    [STUDY] Locust

--- locust-tutorial/prepare_prometheus_EC2.sh
+++ locust-tutorial/prepare_prometheus_EC2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+locust_server=$1
+
 wget https://github.com/afj-solution/public-scripts/raw/master/other/install-docker.sh
 wget https://github.com/afj-solution/public-scripts/raw/master/locust-tutorial/update_prometheus.py
 sudo apt install -y python3-pip python3-pyyaml wget
 chmod +x install-docker.sh
 ./install-docker.sh
 
-python3 update_prometheus.py 
+python3 update_prometheus.py $locust_server
 
 nohup sudo docker run -p 9090:9090 -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus > prometheus.log 2>&1 &
